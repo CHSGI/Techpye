5ad92fea335b00ef73f58b17bc9f276d8885223e
great <greatoladipupo818@gmail.com>
2024-06-11 13:26:15 +0100

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,154 @@
+import Navbar from "./components/NavBar"
+import Hero from "./components/Hero"
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    
+
+    {/* <!-- Developers and Employers --> */}
+    <section className="py-4">
+      <div className="m-auto container-xl lg:container">
+        <div className="grid grid-cols-1 gap-4 p-4 rounded-lg md:grid-cols-2">
+          <div className="p-6 bg-gray-100 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold">For Developers</h2>
+            <p className="mt-2 mb-4">
+              Browse our React jobs and start your career today
+            </p>
+            <a
+              href="/jobs.html"
+              className="inline-block px-4 py-2 text-white bg-black rounded-lg hover:bg-gray-700"
+            >
+              Browse Jobs
+            </a>
+          </div>
+          <div className="p-6 bg-indigo-100 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold">For Employers</h2>
+            <p className="mt-2 mb-4">
+              List your job to find the perfect developer for the role
+            </p>
+            <a
+              href="/add-job.html"
+              className="inline-block px-4 py-2 text-white bg-indigo-500 rounded-lg hover:bg-indigo-600"
+            >
+              Add Job
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    {/* <!-- Browse Jobs --> */}
+    <section className="px-4 py-10 bg-blue-50">
+      <div className="m-auto container-xl lg:container">
+        <h2 className="mb-6 text-3xl font-bold text-center text-indigo-500">
+          Browse Jobs
+        </h2>
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+          {/* <!-- Job Listing 1 --> */}
+          <div className="relative bg-white shadow-md rounded-xl">
+            <div className="p-4">
+              <div className="mb-6">
+                <div className="my-2 text-gray-600">Full-Time</div>
+                <h3 className="text-xl font-bold">Senior React Developer</h3>
+              </div>
+
+              <div className="mb-5">
+               We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript...
+              </div>
+
+              <h3 className="mb-2 text-indigo-500">$70 - $80K / Year</h3>
+
+              <div className="mb-5 border border-gray-100"></div>
+
+              <div className="flex flex-col justify-between mb-4 lg:flex-row">
+                <div className="mb-3 text-orange-700">
+                  <i className="text-lg fa-solid fa-location-dot"></i>
+                  Boston, MA
+                </div>
+                <a
+                  href="job.html"
+                  className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                >
+                 Read More
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* <!-- Job Listing 2 --> */}
+          <div className="relative bg-white shadow-md rounded-xl">
+            <div className="p-4">
+              <div className="mb-6">
+                <div className="my-2 text-gray-600">Remote</div>
+                <h3 className="text-xl font-bold">Front-End Engineer (React)</h3>
+              </div>
+
+              <div className="mb-5">
+               Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion...
+              </div>
+
+              <h3 className="mb-2 text-indigo-500">$70K - $80K / Year</h3>
+
+              <div className="mb-5 border border-gray-100"></div>
+
+              <div className="flex flex-col justify-between mb-4 lg:flex-row">
+                <div className="mb-3 text-orange-700">
+                  <i className="text-lg fa-solid fa-location-dot"></i>
+                  Miami, FL
+                </div>
+                <a
+                  href="job.html"
+                  className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                >
+                 Read More
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* <!-- Job Listing 3 --> */}
+          <div className="relative bg-white shadow-md rounded-xl">
+            <div className="p-4">
+              <div className="mb-6">
+                <div className="my-2 text-gray-600">Remote</div>
+                <h3 className="text-xl font-bold">React.js Developer</h3>
+              </div>
+
+              <div className="mb-5">
+                Are you passionate about front-end development? Join our team in vibrant Brooklyn, NY, and work on exciting projects that make a difference...
+              </div>
+
+              <h3 className="mb-2 text-indigo-500">$70K - $80K / Year</h3>
+
+              <div className="mb-5 border border-gray-100"></div>
+
+              <div className="flex flex-col justify-between mb-4 lg:flex-row">
+                <div className="mb-3 text-orange-700">
+                  <i className="text-lg fa-solid fa-location-dot"></i>
+                  Brooklyn, NY
+                </div>
+                <a
+                  href="job.html"
+                  className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                >
+                 Read More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section className="max-w-lg px-6 m-auto my-10">
+      <a
+        href="jobs.html"
+        className="block px-6 py-4 text-center text-white bg-black rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
+    </>
+  )
+}
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,63 @@
+const navigation = [
+  { name: 'HOME', href: '#' },
+  { name: 'OUR COURSE', href: '#' },
+  { name: 'OUR TEAM', href: '#' },
+  { name: 'WALL OF FAME', href: '#' },
+]
+
+const Hero = () => {
+
+  return (
+    <div className="bg-white">
+      
+
+      <div className="relative px-6 isolate pt-14 lg:px-8">
+        <div
+          className="absolute inset-x-0 overflow-hidden -top-40 -z-10 transform-gpu blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
+        <div className="max-w-2xl py-32 mx-auto sm:py-48 lg:py-56">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Learn the essential tech skill to Navigate today and tomorrow's world
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+            There is a slice of pie for everyone
+            </p>
+            <div className="flex items-center justify-center mt-10 gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-violet-950 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600"
+              >
+                REGISTER FOR COURSE
+              </a>
+            </div>
+          </div>
+        </div>
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default Hero
